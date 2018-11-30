65e825cb79d42c144d06701b0ba9634890313c23
JungHoon Lee <xamp@dbvalley.com>
2018-11-30 10:22:10 +0900

    confirm, alert에도 반영

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import re as re
 import os as os
 
-pattern1 = re.compile(">[a-zA-Z0-9가-힣]+<")
-pattern2 = re.compile("confirm(\"[a-zA-Z0-9가-힣]+\")")
-
-re.compile(pattern2)
+pattern1 = re.compile(r"confirm\(\"([ a-zA-Z0-9가-힣\.\?\!]+)\"\)")
+pattern2 = re.compile(r"alert\(\"([ a-zA-Z0-9가-힣\.\?\!]+)\"\)")
+pattern3 = re.compile(">([ a-zA-Z0-9가-힣\.\?\!]+)<")
 
 path = os.listdir(".")
+
 for files in path:
   fname, ext = os.path.splitext(files)
   if (ext == ".txt"):
@@ -18,14 +18,20 @@ for files in path:
         matchObject1 = re.findall(pattern1, line)
         if matchObject1:
           for match1 in matchObject1:
-            fout.write(re.sub(pattern1, ">'||get_fn('"+match1+"')||'<", line))
+            fout.write(re.sub(pattern1, "confirm(\"'||get_fn('"+match1+"')||'\")", line))
         else:
           re.compile(pattern2)
           matchObject2 = re.findall(pattern2, line)
           if matchObject2:
             for match2 in matchObject2:
-              fout.write(re.sub(pattern2, "confirm(\"'||get_fn('"+match2+"')||'\")\)", line))
+              fout.write(re.sub(pattern2, "alert(\"'||get_fn('"+match2+"')||'\")", line))
           else:
-            fout.write(line)
+            re.compile(pattern3)
+            matchObject3 = re.findall(pattern3, line)
+            if matchObject3:
+              for match3 in matchObject3:
+                fout.write(re.sub(pattern3, ">'||get_fn('"+match3+"')||'<", line))
+            else:
+              fout.write(line)
 
     fout.close()          
